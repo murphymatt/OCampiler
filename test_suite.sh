3dbfae0a165c7d763365b07f78ed48de97f37db0
murphymatt <murphyma@grinnell.edu>
2018-02-02 15:57:01 -0600

    move tests

--- test_suite.sh
+++ test_suite.sh
@@ -2,30 +2,30 @@
 
 # cat test1.txt
 ./ocampiler.native hello world > tmp.txt
-diff tmp.txt test1.txt
+diff tmp.txt tests/test1.txt
 
 # cat test1len.txt
 ./ocampiler.native -length hello world > tmp.txt
-diff tmp.txt test1len.txt
+diff tmp.txt tests/test1len.txt
 
 # cat test2.txt
 ./ocampiler.native hello world    > tmp.txt
-diff tmp.txt test2.txt
+diff tmp.txt tests/test2.txt
 
 # cat test2len.txt
 ./ocampiler.native -length hello world      > tmp.txt
-diff tmp.txt test2len.txt
+diff tmp.txt tests/test2len.txt
 
 # cat test3.txt
 ./ocampiler.native "hello world"    > tmp.txt
-diff tmp.txt test3.txt 
+diff tmp.txt tests/test3.txt 
 
 # cat testhelp.txt
 ./ocampiler.native -help > tmp.txt
-diff tmp.txt testhelp.txt
+diff tmp.txt tests/testhelp.txt
 
 # cat testhelp2.txt
 ./ocampiler.native -help hello world > tmp.txt
-diff tmp.txt testhelp.txt
+diff tmp.txt tests/testhelp.txt
 
 rm tmp.txt
